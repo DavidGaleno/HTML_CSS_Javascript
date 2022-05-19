1696069603dbd9a20598463cdf495a8c945a881d
DavidGaleno <davidgaleno411@gmail.com>
2022-05-18 23:04:03 -0300

    aixos

---
+++ formulario/server.js
@@ -0,0 +1,17 @@
+const bodyParser = require ('body-parser')
+const express = require ('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+
+app.post('/pastaTeste',(request,response)=>{
+    response.send({
+        ...request.body,
+        id:1
+    })
+})
+
+app.use(bodyParser.json())
+app.listen(8080,()=>console.log('Executando...'))
